40721b90f6db159822db272d426e72abfdb20195
AndyAtari <andrewsalteri@gmail.com>
2021-07-05 11:11:49 -0400

    Added Models directory with a SwitchGames Model

---
+++ Models/Switch.cs
@@ -0,0 +1,9 @@
+namespace SwitchGames.Models
+{
+    public class Switch
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Owned { get; set; }
+    }
+}
